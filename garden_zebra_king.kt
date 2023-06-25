89a3f65dcff6ca6a453a22b9fe3ff2420b49168b
budkstlo <136753840+budkstlo@users.noreply.github.com>
2023-06-25 14:07:00 +0700

    Update user permissions

---
+++ garden_zebra_king.kt
@@ -0,0 +1,524 @@
+//1
+fun main() {
+    fightHunger()
+}
+
+//2
+private fun fightHunger() {
+    donateFood()
+    volunteerTime()
+    donateMoney()
+    startCampaign()
+    spreadAwareness()
+    teachNutrition()
+    supportLocalOrganizations()
+    startFundraisingEvents()
+    callLocalPoliticians()
+    initiateFoodDrives()
+    callFoodBanks()
+    signPetitions()
+    writeLetters()
+    offerMentorshipPrograms()
+    createSkypeCourses()
+    joinOrganizations()
+    buildCommunityGardens()
+    organizePotlucks()
+    startYouthGardeningPrograms()
+    findUnderutilizedLand()
+    donateMedicalSupplies()
+    raiseAwarenessWebinars()
+    createTargetedFundraisers()
+    organizeFoodSwaps()
+    volunteerAtFoodPantries()
+    advocateForChange()
+    volunteerWithKids()
+    offerCookingClasses()
+    promoteHealthyEating()
+    createGroceryShoppingPrograms()
+    buildGardensInNursingHomes()
+    feedAnimalsInNeed()
+    sendFoodBaskets()
+    donateVacuumPackedMeals()
+    supportMealDeliveryPrograms()
+    spearheadSchoolGardeningProjects()
+    startFoodRecoveryPrograms()
+    educateOnFoodWasteReduction()
+    provideNutritionalCounseling()
+    setUpFoodPantryLibraries()
+    shopAtLocalFoodCoops()
+    organizeGardeningWorkshops()
+    donateClothingAndSupplies()
+    volunteerAtSoupKitchens()
+    workWithPartnershipGroups()
+    createNutritionTutorials()
+    joinFoodForAllInitiatives()
+    partnerWithFarmsToFightHunger()
+    createFoodCooperativePrograms()
+    advocateForUrbanAgriculture()
+    supportUrbanGardeningProjects()
+    giveGiftCardsToFamiliesInNeed()
+    encourageMealSharingPrograms()
+    mentorYouthFoodEntrepreneurs()
+    connectRestaurantsWithFoodBanks()
+    organizeFarmersMarketsInNeedyAreas()
+    bringCulturalFoodAwarenessInSchools()
+    helpOrganizeFoodDistributionEvents()
+    facilitateFoodSharingCommunities()
+    setUpFoodExchangePrograms()
+    createSchoolGardeningCompetitions()
+    organizeCommunityFridgeNetworks()
+    launchAProjectToHelpHomelessPeople()
+    promoteCookingAndFoodHandlingPrograms()
+    createResourcesForSupportingLocalFarms()
+    createMealsForThoseInNeed()
+    supportVegetableDeliveryBusinesses()
+    useTechnologyToEndHunger()
+    startAGlobalInitiativeToEndHunger()
+    setUpFoodRecoveryNetworks()
+    organizeEducationalToursOnFoodWaste()
+    createFoodBridgesBetweenCountries()
+    startLocalGroceryStorePrograms()
+    createFoodEducationPrograms()
+    buildGardensInHospitalsAndSchools()
+    supportUrbanGardeningCoops()
+    facilitateVolunteerOppurtunitiesForHungerAlleviation()
+    engageCommunitiesInHungerReliefProjects()
+    organizeNutritionAwarenessPrograms()
+    createMealPreparationResourses()
+    startCommunityKitchens()
+    createFoodJusticeAdvocacyPrograms()
+    campaignForWICAndSNAPPrograms()
+    lobbyForUniversalHealthCareAccess()
+    createFoodEducationInitiatives()
+    encourageMealAndNutritionPlanning()
+}
+
+//3
+private fun donateFood() {
+    // TODO: code to donate Food
+}
+
+//4
+private fun volunteerTime() {
+  // TODO: code to volunteer time  
+}
+
+//5
+private fun donateMoney() {
+    // TODO: code to donate Money
+}
+
+//6
+private fun startCampaign (){
+    // TODO: code to start campaign
+}
+
+//7
+private fun spreadAwareness() {
+    // TODO: code to spread awareness
+}
+
+//8
+private fun teachNutrition() {
+    // TODO: code to teach nutrition
+}
+
+//9
+private fun supportLocalOrganizations() {
+    // TODO: code to support local organizations
+}
+
+//10
+private fun startFundraisingEvents() {
+    // TODO: code to start fundraising events
+}
+
+//11
+private fun callLocalPoliticians() {
+    // TODO: code to call local politicians
+}
+
+//12
+private fun initiateFoodDrives() {
+    // TODO: code to initiate food drives
+}
+
+//13
+private fun callFoodBanks() {
+    // TODO: code to call food banks
+}
+
+//14
+private fun signPetitions() {
+    // TODO: code to sign petitions
+}
+
+//15
+private fun writeLetters() {
+    // TODO: code to write letters
+}
+
+//16
+private fun offerMentorshipPrograms() {
+    // TODO: code to offer mentorship programs
+}
+
+//17
+private fun createSkypeCourses() {
+    // TODO: code to create skype courses   
+}
+
+//18
+private fun joinOrganizations() {
+    // TODO: code to join organizations
+}
+
+//19
+private fun buildCommunityGardens() {
+    // TODO: code to build community gardens
+}
+
+//20
+private fun organizePotlucks() {
+    // TODO: code to organize potlucks
+}
+
+//21
+private fun startYouthGardeningPrograms() {
+    // TODO: code to start youth gardening programs
+}
+
+//22
+private fun findUnderutilizedLand() {
+    // TODO: code to find underutilized land
+}
+
+//23
+private fun donateMedicalSupplies() {
+    // TODO: code to donate medical supplies
+}
+
+//24
+private fun raiseAwarenessWebinars() {
+    // TODO: code to raise awareness webinars
+}
+
+//25
+private fun createTargetedFundraisers() {
+    // TODO: code to create targeted fundraisers
+}
+
+//26
+private fun organizeFoodSwaps() {
+    // TODO: code to organize food swaps
+}
+
+//27
+private fun volunteerAtFoodPantries() {
+    // TODO: code to volunteer at food pantries
+}
+
+//28
+private fun advocateForChange() {
+    // TODO: code to advocate for change
+}
+
+//29
+private fun volunteerWithKids() {
+    // TODO: code to volunteer with kids
+}
+
+//30
+private fun offerCookingClasses() {
+    // TODO: code to offer cooking classes
+}
+
+//31
+private fun promoteHealthyEating() {
+    // TODO: code to promote healthy eating
+}
+
+//32
+private fun createGroceryShoppingPrograms() {
+    // TODO: code to create grocery shopping programs
+}
+
+//33
+private fun buildGardensInNursingHomes() {
+    // TODO: code to build gardens in nursing homes
+}
+
+//34
+private fun feedAnimalsInNeed() {
+    // TODO: code to feed animals in need
+}
+
+//35
+private fun sendFoodBaskets() {
+    // TODO: code to send food baskets
+}
+
+//36
+private fun donateVacuumPackedMeals() {
+    // TODO: code to donate vacuum packed meals
+}
+
+//37
+private fun supportMealDeliveryPrograms() {
+    // TODO: code to support meal delivery programs
+}
+
+//38
+private fun spearheadSchoolGardeningProjects() {
+    // TODO: code to spearhead school gardening projects
+}
+
+//39
+private fun startFoodRecoveryPrograms() {
+    // TODO: code to start food recovery programs
+}
+
+//40
+private fun educateOnFoodWasteReduction() {
+    // TODO: code to educate on food waste reduction
+}
+
+//41
+private fun provideNutritionalCounseling() {
+    // TODO: code to provide nutritional counseling
+}
+
+//42
+private fun setUpFoodPantryLibraries() {
+    // TODO: code to set up food pantry libraries
+}
+
+//43
+private fun shopAtLocalFoodCoops() {
+    // TODO: code to shop at local food coops
+}
+
+//44
+private fun organizeGardeningWorkshops() {
+    // TODO: code to organize gardening workshops
+}
+
+//45
+private fun donateClothingAndSupplies() {
+    // TODO: code to donate clothing and supplies
+}
+
+//46
+private fun volunteerAtSoupKitchens() {
+    // TODO: code to volunteer at soup kitchens
+}
+
+//47
+private fun workWithPartnershipGroups() {
+    // TODO: code to work with partnership groups
+}
+
+//48
+private fun createNutritionTutorials() {
+    // TODO: code to create nutrition tutorials
+}
+
+//49
+private fun joinFoodForAllInitiatives() {
+    // TODO: code to join food for all initiatives
+}
+
+//50
+private fun partnerWithFarmsToFightHunger() {
+    // TODO: code to partner with farms to fight hunger
+}
+
+//51
+private fun createFoodCooperativePrograms() {
+    // TODO: code to create food cooperative programs
+}
+
+//52
+private fun advocateForUrbanAgriculture() {
+    // TODO: code to advocate for urban agriculture
+}
+
+//53
+private fun supportUrbanGardeningProjects() {
+    // TODO: code to support urban gardening projects
+}
+
+//54
+private fun giveGiftCardsToFamiliesInNeed() {
+    // TODO: code to give gift cards to families in need
+}
+
+//55
+private fun encourageMealSharingPrograms() {
+    // TODO: code to encourage meal sharing programs
+}
+
+//56
+private fun mentorYouthFoodEntrepreneurs() {
+    // TODO: code to mentor youth food entrepreneurs
+}
+
+//57
+private fun connectRestaurantsWithFoodBanks() {
+    // TODO: code to connect restaurants with food banks
+}
+
+//58
+private fun organizeFarmersMarketsInNeedyAreas() {
+    // TODO: code to organize farmers markets in needy areas
+}
+
+//59
+private fun bringCulturalFoodAwarenessInSchools() {
+    // TODO: code to bring cultural food awareness in schools
+}
+
+//60
+private fun helpOrganizeFoodDistributionEvents() {
+    // TODO: code to help organize food distribution events
+}
+
+//61
+private fun facilitateFoodSharingCommunities() {
+    // TODO: code to facilitate food sharing communities
+}
+
+//62
+private fun setUpFoodExchangePrograms() {
+    // TODO: code to set up food exchange programs
+}
+
+//63
+private fun createSchoolGardeningCompetitions() {
+    // TODO: code to create school gardening competitions
+}
+
+//64
+private fun organizeCommunityFridgeNetworks() {
+    // TODO: code to organize community fridge networks
+}
+
+//65
+private fun launchAProjectToHelpHomelessPeople() {
+    // TODO: code to launch a project to help homeless people
+}
+
+//66
+private fun promoteCookingAndFoodHandlingPrograms() {
+    // TODO: code to promote cooking and food handling programs
+}
+
+//67
+private fun createResourcesForSupportingLocalFarms() {
+    // TODO: code to create resources for supporting local farms
+}
+
+//68
+private fun createMealsForThoseInNeed() {
+    // TODO: code to create meals for those in need
+}
+
+//69
+private fun supportVegetableDeliveryBusinesses() {
+    // TODO: code to support vegetable delivery businesses
+}
+
+//70
+private fun useTechnologyToEndHunger() {
+    // TODO: code to use technology to end hunger
+}
+
+//71
+private fun startAGlobalInitiativeToEndHunger() {
+    // TODO: code to start a global initiative to end hunger
+}
+
+//72
+private fun setUpFoodRecoveryNetworks() {
+    // TODO: code to set up food recovery networks
+}
+
+//73
+private fun organizeEducationalToursOnFoodWaste() {
+    // TODO: code to organize educational tours on food waste
+}
+
+//74
+private fun createFoodBridgesBetweenCountries() {
+    // TODO: code to create food bridges between countries
+}
+
+//75
+private fun startLocalGroceryStorePrograms() {
+    // TODO: code to start local grocery store programs
+}
+
+//76
+private fun createFoodEducationPrograms() {
+    // TODO: code to create food education programs
+}
+
+//77
+private fun buildGardensInHospitalsAndSchools() {
+    // TODO: code to build gardens in hospitals and schools
+}
+
+//78
+private fun supportUrbanGardeningCoops() {
+    // TODO: code to support urban gardening coops
+}
+
+//79
+private fun facilitateVolunteerOppurtunitiesForHungerAlleviation() {
+    // TODO: code to facilitate volunteer oppurtunities for hunger alleviation
+}
+
+//80
+private fun engageCommunitiesInHungerReliefProjects() {
+    // TODO: code to engage communities in hunger relief projects
+}
+
+//81
+private fun organizeNutritionAwarenessPrograms() {
+    // TODO: code to organize nutrition awareness programs
+}
+
+//82
+private fun createMealPreparationResourses() {
+    // TODO: code to create meal preparation resourses
+}
+
+//83
+private fun startCommunityKitchens() {
+    // TODO: code to start community kitchens
+}
+
+//84
+private fun createFoodJusticeAdvocacyPrograms() {
+    // TODO: code to create food justice advocacy programs
+}
+
+//85
+private fun campaignForWICAndSNAPPrograms() {
+    // TODO: code to campaign for WIC and SNAP programs
+}
+
+//86
+private fun lobbyForUniversalHealthCareAccess() {
+    // TODO: code to lobby for universal health care access
+}
+
+//87
+private fun createFoodEducationInitiatives() {
+    // TODO: code to create food education initiatives
+}
+
+//88
+private fun encourageMealAndNutritionPlanning() {
+    // TODO: code to encourage meal and nutrition planning
+}
